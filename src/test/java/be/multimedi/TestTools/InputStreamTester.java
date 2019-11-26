27204d25ea8780691f624f4e85de1f87ddc451fa
Ward <ward@whatever>
2019-11-26 17:51:44 +0100

    updated: TestTools

--- src/test/java/be/multimedi/TestTools/InputStreamTester.java
+++ src/test/java/be/multimedi/TestTools/InputStreamTester.java
@@ -7,47 +7,63 @@ import java.io.InputStream;
  * InputStream tester
  */
 public class InputStreamTester extends InputStream {
-    byte[] bytes = null; // buffer
-    int index = 0; // index, current(reading) position in buffer
-    String nextLine = null; // next/split of input
+   byte[] bytes = null; // buffer
+   int index = 0; // index, current(reading) position in buffer
+   String nextLine = null; // next/split of input
 
-    public void setInputString(String text) {
-        if (text != null) {
-            //# split input after newline \n
-            int index = text.indexOf("\n");
-            if(index>=0) {
-                nextLine = text.substring(index+1);
-                text=text.substring(0, index+1);
-            }else {
-                nextLine = null;
-            }
-            //# add newline if none exists
-            if(!text.contains("\n")) text+= "\n";
-            //# set buffer
-            bytes = text.getBytes();
-        }
-        else{
+   /**
+    * Setter for the current input/data to process
+    *
+    * @param text the text to use as input
+    */
+   public void setInputString(String text) {
+      if (text != null) {
+         //# split input after newline \n
+         int index = text.indexOf("\n");
+         if (index >= 0) {
+            nextLine = text.substring(index + 1);
+            text = text.substring(0, index + 1);
+         } else {
             nextLine = null;
-            bytes = null;
-        }
-        //# set index
-        index = 0;
-    }
+         }
+         //# add newline if none exists
+         if (!text.contains("\n")) text += "\n";
+         //# set buffer
+         bytes = text.getBytes();
+      } else {
+         nextLine = null;
+         bytes = null;
+      }
+      //# set index
+      index = 0;
+   }
 
-    public void reset(){
-        setInputString(null);
-    }
+   /**
+    * Reset input
+    */
+   public void reset() {
+      setInputString(null);
+   }
 
-    public boolean isEnded(){
-        if( bytes == null ) return true;
-        return index >= bytes.length && nextLine == null;
-    }
+   /**
+    * Has every byte been read?
+    *
+    * @return if everything has been read this returns true, otherwise false
+    */
+   public boolean isEnded() {
+      if (bytes == null) return true;
+      return index >= bytes.length && nextLine == null;
+   }
 
-    @Override
-    public int read() throws IOException {
-        if (bytes != null && index < bytes.length && index >= 0) // are we in the buffer?
-            return bytes[index++]; // return buffer at index and continue reading(++)
-        setInputString(nextLine); // prepare next line
-        return -1; // return end of buffer
-    }
+   @Override
+   /**
+    * Read next byte
+    * @return next byte in stream or -1(when the stream ended)
+    */
+   public int read() throws IOException {
+      if (bytes != null && index < bytes.length && index >= 0) // are we in the buffer?
+         return bytes[index++]; // return buffer at index and continue reading(++)
+      setInputString(nextLine); // prepare next line
+      return -1; // return end of buffer
+   }
 }
--- src/test/java/be/multimedi/TestTools/SystemInOutTester.java
+++ src/test/java/be/multimedi/TestTools/SystemInOutTester.java
@@ -7,66 +7,71 @@ import org.junit.jupiter.api.BeforeAll;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 /**
  * A class to help build test-cases that need to test algorithms using System streams (in, out, err)
  */
 public class SystemInOutTester {
-    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private static final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private static final InputStreamTester inContent = new InputStreamTester();// = new ByteArrayInputStream(null);
-    private static final PrintStream originalOut = System.out;
-    private static final PrintStream originalErr = System.err;
-    private static final InputStream originalIn = System.in;
+   private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+   private static final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+   private static final InputStreamTester inContent = new InputStreamTester();// = new ByteArrayInputStream(null);
+   private static final PrintStream originalOut = System.out;
+   private static final PrintStream originalErr = System.err;
+   private static final InputStream originalIn = System.in;
 
-    /**
-     * Set system-streams so they can be used for testing
-     */
-    @BeforeAll
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-        System.setIn(inContent);
-    }
+   /**
+    * Set system-streams so they can be used for testing
+    */
+   @BeforeAll
+   public void setUpStreams() {
+      System.setOut(new PrintStream(outContent));
+      System.setErr(new PrintStream(errContent));
+      System.setIn(inContent);
+   }
 
-    /**
-     * Set Input of the user
-     */
-    public void setInput(String data) {
-        inContent.setInputString(data);
-    }
+   /**
+    * Set Input of the user
+    *
+    * @param data The data to use as input.
+    */
+   public void setInput(String data) {
+      inContent.setInputString(data);
+   }
 
-    /**
-     * Get Output of the algorithm
-     */
-    public String getOutput() {
-        return outContent.toString();
-    }
+   /**
+    * Get Output of the algorithm
+    *
+    * @return returns the output stream content
+    */
+   public String getOutput() {
+      return outContent.toString();
+   }
 
-    /**
-     * Get Error of the algorithm
-     */
-    public String getError() {
-        return errContent.toString();
-    }
+   /**
+    * Get Error of the algorithm
+    *
+    * @return returns the error stream content
+    */
+   public String getError() {
+      return errContent.toString();
+   }
 
-    /**
-     * Restore system-streams so they can be used as normal
-     */
-    @AfterAll
-    public void restoreStreams() {
-        System.setOut(originalOut);
-        System.setErr(originalErr);
-        System.setIn(originalIn);
-    }
+   /**
+    * Restore system-streams so they can be used as normal
+    */
+   @AfterAll
+   public void restoreStreams() {
+      System.setOut(originalOut);
+      System.setErr(originalErr);
+      System.setIn(originalIn);
+   }
 
-    /**
-     * Reset testing Streams so no test-output remains
-     */
-    @AfterEach
-    public void resetStreams() {
-        inContent.reset();
+   /**
+    * Reset testing Streams so no test-output remains
+    */
+   @AfterEach
+   public void resetStreams() {
+      inContent.reset();
         /* //# Reset remaining scanner input?
         //Scanner keyb = new Scanner(System.in);
         while(true){
@@ -77,13 +82,16 @@ public class SystemInOutTester {
                 break;
             }
         }// */
-        outContent.reset();
-        errContent.reset();
-    }
+      outContent.reset();
+      errContent.reset();
+   }
 
-    public void printStatus(){
-        originalOut.println("Input Finished: " + inContent.isEnded());
-        originalOut.println("Output: " + getOutput());
-        originalErr.println("Errors: " + getError());
-    }
+   /**
+    * Print status and content of Streams on the original OutputStream (of System.in)
+    */
+   public void printStatus() {
+      originalOut.println("Input Finished: " + inContent.isEnded());
+      originalOut.println("Output: " + getOutput());
+      originalErr.println("Errors: " + getError());
+   }
 }
